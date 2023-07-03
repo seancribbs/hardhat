4e53751fe95bf87fc800764302d10b12d9d9e5b1
Sean Cribbs <sean@fable.app>
2023-07-03 15:54:02 -0500

    Setup hex and CI

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Hardhat.MixProject do
       version: "0.1.0-alpha.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "An opinionated, production-ready HTTP client for Elixir services.",
+      package: package()
     ]
   end
 
@@ -20,7 +22,18 @@ defmodule Hardhat.MixProject do
   defp deps do
     [
       {:tesla, "~> 1.7"},
-      {:finch, "~> 0.16.0"}
+      {:finch, "~> 0.16.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.3.0", only: :dev}
+    ]
+  end
+
+  defp package do
+  [
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/seancribbs/hardhat"
+      }
     ]
   end
 end
