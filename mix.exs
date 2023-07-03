b89f8b2612f12109d06f7082e3089f293215dd4b
Sean Cribbs <sean@fable.app>
2023-07-03 15:40:02 -0500

    Set version to alpha

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hardhat.MixProject do
   def project do
     [
       app: :hardhat,
-      version: "0.1.0",
+      version: "0.1.0-alpha.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
