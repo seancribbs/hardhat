c01e5192301f995ba905240b1c27c37a55f0ef7b
Sean Cribbs <sean@fable.app>
2023-07-03 14:55:58 -0500

    Add initial dependencies

--- mix.exs
+++ mix.exs
@@ -14,15 +14,14 @@ defmodule Hardhat.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:tesla, "~> 1.7"},
+      {:finch, "~> 0.16.0"}
     ]
   end
 end
