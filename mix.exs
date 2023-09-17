8699dfbe40044d545e054c1911c2b04ebf48f9a9
Sean Cribbs <sean@fable.app>
2023-09-17 17:38:41 -0500

    Bump version to 1.0.0-rc.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hardhat.MixProject do
   def project do
     [
       app: :hardhat,
-      version: "1.0.0-rc.1",
+      version: "1.0.0-rc.2",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
