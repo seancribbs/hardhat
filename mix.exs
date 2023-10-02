4180be5d7dd7912a007fb6b6eafa7f87367907cd
Sean Cribbs <sean@fable.app>
2023-10-02 16:56:33 -0500

    Bump to 1.0.0-rc.4 and testing release workflow

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hardhat.MixProject do
   def project do
     [
       app: :hardhat,
-      version: "1.0.0-rc.3",
+      version: "1.0.0-rc.4",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
