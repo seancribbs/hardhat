53e8e8c490f64ead3c42a7900302327a659d562d
Sean Cribbs <sean@fable.app>
2023-11-26 17:40:47 -0600

    Release 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hardhat.MixProject do
   def project do
     [
       app: :hardhat,
-      version: "1.0.0-rc.4",
+      version: "1.0.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -29,13 +29,12 @@ defmodule Hardhat.MixProject do
       {:tesla, "~> 1.7"},
       {:finch, "~> 0.16.0"},
       {:telemetry, "~> 1.2"},
-      {:opentelemetry_tesla, "~> 2.2"},
+      {:opentelemetry_tesla, "~> 2.3.0"},
       {:opentelemetry, "~> 1.3", only: :test},
       {:opentelemetry_process_propagator, "~> 0.2.2"},
       {:fuse, "~> 2.5"},
       {:regulator, "~> 0.5.0"},
       {:deadline, "~> 0.7.1"},
-      # {:recon, "~> 2.5.4"},
       {:ex_doc, "~> 0.30.6", only: :dev, runtime: false},
       {:dialyxir, "~> 1.4.0", only: :dev, runtime: false},
       {:bypass, "~> 2.1.0", only: :test}
